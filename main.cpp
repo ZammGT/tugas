5746fc2d2d4e4239fff135d862ef1b5b9f602478
Azam Ardiansyah Putra <azam02ardi99@gmail.com>
2026-09-03 12:54:18 +0700

    new

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string a,pass;
+    cout<<"Masukkan Nama : ";
+    cin>>a;
+    cout<<"Masukkan Password : ";
+    cin>>pass;
+    if(a =="azam" && pass=="2026"){
+        cout<<"Hore Berhasil Login Sebagai Azam";
+    }
+    else if(a =="yanto" && pass=="1998"){
+        cout<<"Hore Barhasil Login Sebagai Yanto";
+    }
+    else
+    cout<<"Lu Sape Mpruy";
+    return 0;
+}
